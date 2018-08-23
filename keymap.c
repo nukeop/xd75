027f5c469da628df67465de529bbf13469e7191a
nukeop <nukeop@gumblert.tech>
2018-08-23 12:41:40 +0200

    layout fix

--- keymap.c
+++ keymap.c
@@ -34,7 +34,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QW] = { /* QWERTY */
-    { KC_GESC,        KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_HOME, KC_END,  TD(TAP9), TD(TAP8), TD(TAP7), KC_PGDOWN, KC_PGUP, KC_PSCREEN, KC_MINUS, KC_EQUAL, KC_BSPACE },
+    { KC_GESC,        KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_HOME, KC_END,  TD(TAP7), TD(TAP8), TD(TAP9), KC_PGDOWN, KC_PGUP, KC_PSCREEN, KC_MINUS, KC_EQUAL, KC_BSPACE },
     { KC_TAB,         KC_Q,        KC_W,        KC_E,      KC_R,    KC_T,    TD(TAP4), TD(TAP5), TD(TAP6), KC_Y,      KC_U,    KC_I,       KC_O,     KC_P,     KC_QUOT   },
     { ALL_T(_______), KC_A,        KC_S,        KC_D,      KC_F,    KC_G,    TD(TAP1), TD(TAP2), TD(TAP3), KC_H,      KC_J,    KC_K,       KC_L,     KC_SCLN,  KC_ENT    },
     { KC_LSPO,        KC_Z,        KC_X,        KC_C,      KC_V,    KC_B,    TD(TAP0), KC_UP,    KC_INS,   KC_N,      KC_M,    KC_COMM,    KC_DOT,   KC_SLSH,  KC_RSPC   },
