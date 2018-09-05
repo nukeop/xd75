c28a0a047d6fd9a3b94b9a36f37e0512b0e9c8a9
nukeop <nukeop@gumblert.tech>
2018-09-05 11:36:59 +0200

    mod tap for tab

--- keymap.c
+++ keymap.c
@@ -3,11 +3,13 @@
 #define _QW 0
 #define _FN 1
 
+#define TAB MT(MOD_LCTL, KC_TAB)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = { /* QWERTY */
            { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_7,    KC_8,     KC_9,   KC_0,   KC_MINUS, KC_EQUAL, KC_PSCR, KC_BSPACE },
            { KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_INS,  KC_HOME, KC_PGUP,  KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,    KC_QUOT   },
-           { KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_DEL,  KC_END,  KC_PGDN,  KC_H,   KC_J,   KC_K,     KC_L,     KC_SCLN, KC_ENT    },
+           { TAB,     KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_DEL,  KC_END,  KC_PGDN,  KC_H,   KC_J,   KC_K,     KC_L,     KC_SCLN, KC_ENT    },
            { KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_LBRC, KC_RBRC, KC_BSLS,  KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, KC_RSPC   },
            { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_SPC, KC_SPC, KC_RALT, KC_RGUI, KC_RCTRL, KC_SPC, KC_SPC, KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT   },
   },
